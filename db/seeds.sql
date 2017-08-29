283900ae3e8fe8ef18bfdee6ddebf0fa8edad897
Elton Hsu <eltonhsu@MacBook-Pro-3.local>
2017-08-29 14:58:55 -0700

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Cheeseburger', true, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken', false, false, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('Hamburger', true, false, false, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('McChicken', false, true, true, false);
+INSERT INTO burgers (name, meat, onion, cheese, devoured) VALUES ('BigMac', true, true, true, false);
